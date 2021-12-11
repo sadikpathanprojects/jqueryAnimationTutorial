9940974959237f4c24125505affd9e545dcdca04
sadikpathan9876 <65956063+sadikpathanprojects@users.noreply.github.com>
2021-12-11 22:02:23 +0530

    Delete hideandshow.js

--- hideandshow.js
+++
@@ -1,5 +0,0 @@
-alert("I request you to open this page in Desktop Site Mode. Otherwise you  will not be able to see it properly.")
-$("#button1").click(function(){
-  $("#hw").hide();
-  $("#refresh").text("Refresh the page if you want to hide 'hello world' again. ")
-});
